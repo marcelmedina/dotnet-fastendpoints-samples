07c3690363b59b0630bceb42d8e043de13cb7d8f
Marcel Medina <marcel.net@gmail.com>
2025-08-12 23:29:55 +1200

    Add Swagger support with FastEndpoints.Swagger
    Updated `FastEndpointsSamples.csproj` to include the
    `FastEndpoints.Swagger` package for Swagger documentation.
    Reordered `using` statements in `Program.cs` and
    refactored the FastEndpoints service configuration to
    include a call to `.SwaggerDocument()`. Updated the
    application pipeline to use `.UseSwaggerGen()` for
    proper Swagger integration.

--- FastEndpointsSamples/Program.cs
+++ FastEndpointsSamples/Program.cs
@@ -1,12 +1,17 @@
-using System.Reflection;
 using FastEndpoints;
+using FastEndpoints.Swagger;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder();
-builder.Services.AddFastEndpoints(o =>
-{
-    o.Assemblies = [Assembly.GetExecutingAssembly()]; // To avoid: System.InvalidOperationException: 'FastEndpoints was unable to find any endpoint declarations!'
-});
+builder.Services
+    .AddFastEndpoints(o =>
+    {
+        o.Assemblies = [Assembly.GetExecutingAssembly()]; // To avoid: System.InvalidOperationException: 'FastEndpoints was unable to find any endpoint declarations!'
+    })
+    .SwaggerDocument(); //define a swagger document
 
 var app = builder.Build();
-app.UseFastEndpoints();
+app
+    .UseFastEndpoints()
+    .UseSwaggerGen();
 app.Run();
